b78c4bcb956e69c8753bd2937ee63162e645c916
Bernardo Farrobinha <bernardofarrobinha1@gmail.com>
2023-01-06 15:16:33 +0000

    Program and Readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Hide-folder
+
+This is a simple script to hide a folder in Windows.
+
+## Usage
+
+### Create a `Private` folder
+
+1. Run the command `py hide-folder.py`.
+2. The script will be create a new folder in the same directory called `Private`, if it doesn't exist.
+3. Put the files you want to hide in the `Private` folder.
+
+### Hide the `Private` folder
+
+1. Run the command `py hide-folder.py` again.
+2. The script will hide the `Private` folder.
+
+### Unhide the `Private` folder
+
+1. To unhide the folder, run the command `py hide-folder.py` again.
+2. The script will be ask you for the password to unhide the folder.
+3. Enter the password and the `Private` folder will be unhidden.
+
+## Questions
+
+### How to set your own password?
+
+1. Open the script with a text editor.
+2. Change the value of the variable `master_password` to your own password.
+
+### How to change the name of the folder to hide?
+
+1. Open the script with a text editor.
+2. Change the value of the variable `folder_name` to your own name.
+
+### How to change the name of the hidden folder?
+
+1. Open the script with a text editor.
+2. Change the value of the variable `hidden_folder_name` to your own name.
---
+++ hide-folder.py
@@ -0,0 +1,40 @@
+import os
+import getpass
+
+master_password = "YOUR_PASSWORD_HERE"
+folder_name = "Private" # Name of the folder to lock
+hidden_folder_name = "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" # Rename the folder to this
+
+def lock_folder():
+    # Rename the folder and set system and hidden attributes
+    os.rename(folder_name, hidden_folder_name)
+    os.system("attrib +h +s \""+hidden_folder_name+"\"")
+    print("Folder locked")
+
+def unlock_folder():
+    # Prompt for password
+    password = getpass.getpass("Enter password to unlock folder: ")
+
+    if password == master_password:
+        # Remove system and hidden attributes and rename the folder back
+        os.system("attrib -h -s \""+hidden_folder_name+"\"")
+        os.rename(hidden_folder_name, folder_name)
+        print("Folder unlocked successfully")
+    else:
+        print("Invalid password")
+
+# Check if the user is an administrator
+if os.path.exists(hidden_folder_name):
+    unlock_folder()
+else:
+    # Check if the folder exists
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+        print("Private folder created successfully")
+    else:
+        # Prompt user to confirm they want to lock the folder
+        answer = input("Are you sure you want to lock the folder? (Y/N) ")
+        if answer.lower() == "y":
+            lock_folder()
+        else:
+            print("Operation cancelled")
